9faeb67891db71014e2276e9b683ba8378239fb2
KarimaTouhami <113696378+KarimaTouhami@users.noreply.github.com>
2023-07-31 19:24:03 +0100

    Update product_model.dart

--- lib/model/product_model.dart
+++ lib/model/product_model.dart
@@ -1,21 +1,77 @@
-import '../config/asset_image_paths.dart';
+import 'package:flutter/material.dart';
+import 'package:woocommerce_api/woocommerce_api.dart';
 
-class AllProduct {
-  String? imagePng;
-  String? price;
-  String? name;
-  String? discount;
+void main() => runApp(MyApp());
 
-  AllProduct({this.imagePng, this.price, this.name, this.discount});
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'WooCommerce API Demo'),
+    );
+  }
 }
 
-List<AllProduct> allProductList = [
-  AllProduct(name: 'Lockies Shirt', imagePng: AssetImagePaths.proDetail),
-  AllProduct(name: 'Black Blazer', imagePng: AssetImagePaths.secondgril),
-  AllProduct(name: 'Blue Blazer', imagePng: AssetImagePaths.mansecod),
-  AllProduct(name: 'Blue Shirt', imagePng: AssetImagePaths.manthard),
-  AllProduct(name: 'Lockies Shirt', imagePng: AssetImagePaths.proDetail),
-  AllProduct(name: 'Black Blazer', imagePng: AssetImagePaths.secondgril),
-  AllProduct(name: 'Blue Blazer', imagePng: AssetImagePaths.mansecod),
-  AllProduct(name: 'Blue Shirt', imagePng: AssetImagePaths.manthard),
-];
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  WooCommerceAPI wooCommerceAPI = WooCommerceAPI(
+    url: "https://www.italiamachines.com",
+    consumerKey: "ck_e97c1203e673f06215aae7f42c436148d182a4eb",
+    consumerSecret: "cs_be09c2e27360d5b7eb635c82ee72f440289d7cda",
+  );
+
+  Future<List<dynamic>> _getProducts() async {
+    // Get data using the "products" endpoint
+    var products = await wooCommerceAPI.getAsync("products");
+    return products;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: FutureBuilder(
+        future: _getProducts(),
+        builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+          if (snapshot.hasData) {
+            // Create a list of products
+            return ListView.separated(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    child: Image.network(
+                      snapshot.data![index]["images"][0]["src"],
+                    ),
+                  ),
+                  title: Text(snapshot.data![index]["name"]),
+                  subtitle: Text("Buy now for \$ " + snapshot.data![index]["price"]),
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) => Divider(height: 1),
+            );
+          }
+
+          // Show a circular progress indicator while loading products
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+}
